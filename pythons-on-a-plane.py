8ca74af81ffadc0fd1fd4072883463e56f0bb6ec
Owen <ocooke@ualberta.ca>
2022-01-15 13:25:34 -0700

    init pygame, set screen size

--- pythons-on-a-plane.py
+++ pythons-on-a-plane.py
@@ -3,8 +3,23 @@
 import pygame
 from pygame.locals import *
 
+# CONSTANT values like colours here
+SKYBLUE = (135, 206, 235)
+
+# Create vars for screen size and width/height of screen
+size = 980, 660
+width, height = size
+
+# Initialize pygame and set a screen variable
+pygame.init()
+screen = pygame.display.set_mode(size)
+
+# Main game loop
+running = True
 while running:
+	# Event loop (for KEYDOWN, etc.)
     for event in pygame.event.get():
+    	# If user hits the x button on pygame window
         if event.type == QUIT:
             running = False
 

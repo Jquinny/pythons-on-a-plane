9b2187e65a72d23a727c5b6f46d6911d84e73a1e
Eric Xiong <exiong@ualberta.ca>
2022-01-15 13:28:51 -0700

    Test for character

--- pythons-on-a-plane.py
+++ pythons-on-a-plane.py
@@ -1,11 +1,23 @@
 # FIXME: Fancy Header Here Later
 
 import pygame
+from sys import exit
 from pygame.locals import *
+pygame.init()
+pygame.display.set_caption('Pythons on a Plane')
+# Create vars for screen size and width/height of screen
+size = 980, 660
+width, height = size
 
-while running:
+# Initialize pygame and set a screen variable
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+while True:
     for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(60)
 
-pygame.quit()

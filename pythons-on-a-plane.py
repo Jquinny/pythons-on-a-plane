917044dc23634ef2015c47fe083128f1168ab81b
Eric Xiong <exiong@ualberta.ca>
2022-01-15 15:04:45 -0700

    aminations

--- pythons-on-a-plane.py
+++ pythons-on-a-plane.py
@@ -7,21 +7,34 @@ from pygame.locals import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.original = pygame.image.load('pythons-on-a-plane/graphics/plane/Fly_(1).png')
-        self.image = pygame.transform.scale(self.original, (232,159))
+        self.original1 = pygame.image.load('pythons-on-a-plane/graphics/plane/Fly_(1).png')
+        self.original2 = pygame.image.load('pythons-on-a-plane/graphics/plane/Fly_(2).png')
+        self.animationState = 0
+        self.image = pygame.transform.scale(self.original1, (232,159))
         self.rect = self.image.get_rect()
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rect.x += -10
-        elif keys[pygame.K_w]:
+        if keys[pygame.K_w]:
             self.rect.y += -10
-        elif keys[pygame.K_s]:
+        if keys[pygame.K_s]:
             self.rect.y += 10
-        elif keys[pygame.K_d]:
+        if keys[pygame.K_d]:
             self.rect.x += 10
+    def player_animation(self):
+        if self.animationState == 0:
+            self.image = pygame.transform.scale(self.original2, (232,159))
+            self.animationState = 1
+        elif self.animationState == 1:
+            self.image = pygame.transform.scale(self.original1, (232,159))
+            self.animationState = 0
+        
+
+        
     def update(self):
         self.player_input()
+        self.player_animation()
             
 
 pygame.init()

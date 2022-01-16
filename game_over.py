487a65d4bb44a81907768e4498e0389d4dedc058
Zack Dorward <35910048+zdorward@users.noreply.github.com>
2022-01-16 03:46:20 -0700

    i should be asleep

--- game_over.py
+++ game_over.py
@@ -1,4 +1,4 @@
-#LEADERBOARD MENU, COMPATIBILE WITH FIREBASE
+#GAME OVER SCREEN
 
 import game_database
 import pygame
@@ -6,10 +6,10 @@ from pygame.locals import *
 
 def game_over_screen(screen, score):
     font = pygame.font.Font(None, 32)
-    input_box = pygame.Rect(545, 440, 140, 32)
+    input_box = pygame.Rect(545, 540, 140, 32)
     color_inactive = pygame.Color('lightskyblue3')
     color_active = pygame.Color('dodgerblue2')
-    color = color_inactive
+    color = color_inactive 
     active = False
     text = ''
     done = False
@@ -41,7 +41,7 @@ def game_over_screen(screen, score):
         
         background = pygame.transform.scale(background, (1280, 1280)) # 1280, 1280
         screen.blit(background, (0, 0))
-        add_text(screen)
+        add_text(screen, score)
         # Render the current text.
         txt_surface = font.render(text, True, color)
         # Resize the box if the text is too long.
@@ -55,22 +55,27 @@ def game_over_screen(screen, score):
         pygame.display.flip()
         
 
-def add_text(screen):
+def add_text(screen, score):
     pos = (390, 220)
     font = pygame.font.Font("graphics/bubble.ttf", 96)
     font_img = font.render('GAME OVER', True, (255, 255, 255))
     rect = font_img.get_rect(topleft=pos)
     screen.blit(font_img, rect)
 
-    pos = (500, 400)
+    pos = (450, 360)
+    font = pygame.font.Font("graphics/bubble.ttf", 48)
+    font_img = font.render(f'YOUR SCORE:', True, (255, 255, 255))
+    rect = font_img.get_rect(topleft=pos)
+    screen.blit(font_img, rect)
+
+    pos = (770, 360)
+    font = pygame.font.Font("graphics/bubble.ttf", 48)
+    font_img = font.render(f'{score}', True, ('dodgerblue2'))
+    rect = font_img.get_rect(topleft=pos)
+    screen.blit(font_img, rect)
+
+    pos = (500, 500)
     font = pygame.font.Font(None, 32)
     font_img = font.render('Enter your username below', True, (255, 255, 255))
     rect = font_img.get_rect(topleft=pos)
     screen.blit(font_img, rect)
-
-
-    
-
-    
-
-
